971462a5d542bea792614936b7fd668c2fea7fc0
Shivansh <shivraj@gmail.com>
2026-05-04 16:58:43 +0530

    add production bundle obfuscation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
+import WebpackObfuscator from "webpack-obfuscator";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new WebpackObfuscator({
+          compact: true,
+          identifierNamesGenerator: "hexadecimal",
+          rotateStringArray: true,
+          selfDefending: true,
+          simplify: true,
+          stringArray: true,
+          stringArrayThreshold: 0.75,
+          transformObjectKeys: true,
+        })
+      );
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
